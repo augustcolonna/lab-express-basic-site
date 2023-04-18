19df7472256a2df208b44c095353ec561b53198c
Gus <august.colonna@gmail.com>
2023-04-18 13:45:31 +0200

    solved lab

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (request, response) => {
+    response.sendFile(__dirname + '/views/works.html')
+})
+
+app.get('/gallery', (request, response) => {
+    response.sendFile(__dirname + '/views/gallery.html')
+})
+
+app.get('/styles', (request, response) => {
+    response.sendFile(__dirname + '/public/styles.css')
+})
+
+app.listen(3001, () => {
+    console.log('We are in port 3001')
+});
